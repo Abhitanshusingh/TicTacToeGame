ad109f11b45feea76e34a7bd83bdb92a20f3269e
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-29 19:19:20 +0530

    Toss to check who play first

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
+printf "Welcome to tic tac toe game \n"
 declare -a board
 board=(" " " " " " " " " " " " " " " " " ")
 function displayBoard()
 {
-printf "Welcome to tic tac toe game \n"
 printf "\t\t\t      |     |     \n"
 printf "\t\t\t   ${board[0]}  |  ${board[1]}  |  ${board[2]}  \n"
 printf "\t\t\t _____|_____|_____\n"
@@ -12,10 +12,11 @@ printf "\t\t\t  ${board[3]}   |  ${board[4]}  |  ${board[5]}  \n"
 printf "\t\t\t _____|_____|_____\n"
 printf "\t\t\t      |     |     \n"
 printf "\t\t\t  ${board[6]}   |  ${board[7]}  |  ${board[8]}  \n"
-
 }
+function assiningLetter
+{
 #GENERATING RANDOM VALUE TO ASSIGN PLAYER O OR X
-random==$((RANDOM%2+1))
+random=$((RANDOM%2+1))
 case $random in
 	1)
 		player="X"
@@ -26,4 +27,18 @@ case $random in
 		computer="X"
 		;;
 esac
-
+}
+function checkToss
+{
+#GENERATING RANDOM VALUE 1 FOR PALYER 2 FOR COMPUTER
+toss=$((RANDOM%2+1))
+case $toss in
+	1)
+		echo "Player play first"
+		;;
+	2)
+		echo "Computer play first"
+		;;
+esac
+}
+displayBoard
