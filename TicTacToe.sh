275ea376a29ab22979446f8d2ca5566d1f4f4064
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-29 18:45:35 +0530

    MODIFIED: Assign O or X to player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -x
 declare -a board
+board=(" " " " " " " " " " " " " " " " " ")
 function displayBoard()
 {
 printf "Welcome to tic tac toe game \n"
-printf "\t\t\t     |     |     \n"
-printf "\t\t\t   ${board[0]}  |  ${board[1]}   |  ${board[3]}  \n"
-printf "\t\t\t_____|_____|_____\n"
-printf "\t\t\t     |     |     \n"
-printf "\t\t\t  ${board[4]}   |  ${board[5]}   |  ${board[6]}  \n"
-printf "\t\t\t_____|_____|_____\n"
-printf "\t\t\t     |     |     \n"
-printf "\t\t\t  ${board[7]}   |  ${board[8]}   |  ${board[9]}  \n"
+printf "\t\t\t      |     |     \n"
+printf "\t\t\t   ${board[0]}  |  ${board[1]}  |  ${board[2]}  \n"
+printf "\t\t\t _____|_____|_____\n"
+printf "\t\t\t      |     |     \n"
+printf "\t\t\t  ${board[3]}   |  ${board[4]}  |  ${board[5]}  \n"
+printf "\t\t\t _____|_____|_____\n"
+printf "\t\t\t      |     |     \n"
+printf "\t\t\t  ${board[6]}   |  ${board[7]}  |  ${board[8]}  \n"
 
 }
-displayBoard
+#GENERATING RANDOM VALUE TO ASSIGN PLAYER O OR X
+random==$((RANDOM%2+1))
+case $random in
+	1)
+		player="X"
+		computer="O"
+		;;
+	2)
+		player="O"
+		computer="X"
+		;;
+esac
+
