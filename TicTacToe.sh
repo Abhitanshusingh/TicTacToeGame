cbbb3569f2d904ccd6e7d6d952e2326a18b711a1
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-01 15:10:08 +0530

    MODIFIED: Toss who play first and improve indentation

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,44 +1,48 @@
 #!/bin/bash -x
-printf "Welcome to tic tac toe game \n"
+printf "\t\t\tWelcome to tic tac toe game \n"
 declare -a board
 board=(" " " " " " " " " " " " " " " " " ")
 function displayBoard()
 {
-printf "\t\t\t      |     |     \n"
-printf "\t\t\t   ${board[0]}  |  ${board[1]}  |  ${board[2]}  \n"
-printf "\t\t\t _____|_____|_____\n"
-printf "\t\t\t      |     |     \n"
-printf "\t\t\t  ${board[3]}   |  ${board[4]}  |  ${board[5]}  \n"
-printf "\t\t\t _____|_____|_____\n"
-printf "\t\t\t      |     |     \n"
-printf "\t\t\t  ${board[6]}   |  ${board[7]}  |  ${board[8]}  \n"
+	printf "\t\t\t      |     |     \n"
+	printf "\t\t\t   ${board[0]}  |  ${board[1]}  |  ${board[2]}  \n"
+	printf "\t\t\t _____|_____|_____\n"
+	printf "\t\t\t      |     |     \n"
+	printf "\t\t\t  ${board[3]}   |  ${board[4]}  |  ${board[5]}  \n"
+	printf "\t\t\t _____|_____|_____\n"
+	printf "\t\t\t      |     |     \n"
+	printf "\t\t\t  ${board[6]}   |  ${board[7]}  |  ${board[8]}  \n"
 }
-function assiningLetter
+function assiningLetter()
 {
-#GENERATING RANDOM VALUE TO ASSIGN PLAYER O OR X
-random=$((RANDOM%2+1))
-case $random in
-	1)
-		player="X"
-		computer="O"
-		;;
-	2)
-		player="O"
-		computer="X"
-		;;
-esac
+	#GENERATING RANDOM VALUE TO ASSIGN PLAYER O OR X
+	random=$((RANDOM%2+1))
+	case $random in
+		1)
+			player="X"
+			computer="O"
+			;;
+		2)
+			player="O"
+			computer="X"
+			;;
+	esac
+	checkToss
 }
-function checkToss
+function checkToss()
 {
-#GENERATING RANDOM VALUE 1 FOR PALYER 2 FOR COMPUTER
-toss=$((RANDOM%2+1))
-case $toss in
-	1)
-		echo "Player play first"
-		;;
-	2)
-		echo "Computer play first"
-		;;
-esac
+	#GENERATING RANDOM VALUE 1 FOR PALYER 2 FOR COMPUTER
+	toss=$((RANDOM%2+1))
+	case $toss in
+		1)
+			printf "Player play first\n"
+			printf "Player assinged: $player\nComputer assinged: $computer\n"
+			;;
+		2)
+			printf "Computer play first\n"
+			printf "Computer assinged: $computer\nPlayer assinged: $player\n"
+			;;
+	esac
 }
+assiningLetter
 displayBoard
